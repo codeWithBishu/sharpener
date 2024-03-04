7a5ef2fa532052b6a649b6e166b80061d719444e
codeWithBishu <bisu311297@gmail.com>
2024-03-04 16:39:33 +0530

    first assignment uploaded

---
+++ index.js
@@ -0,0 +1,12 @@
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+const headerDiv = document.getElementById("header")
+headerDiv.style.backgroundColor = 'green'
+headerDiv.style.borderBottom = "3px solid orange"
+
+const basket = document.getElementById("basket-heading")
+  basket.style.color = "green"
+
+const thank = document.getElementById("thanks");
+thank.innerHTML = "<p>Please visit us again</p>"
